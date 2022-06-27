379f15a8b3ee8158d7092f2daf20374fb250774b
Orestes <oresmendez@gmail.com>
2022-06-27 12:01:16 -0500

    actualizando el sass

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 
 const menu = document.querySelector('.fa-bars');
 menu.addEventListener('click', () => {
-    console.log('click');
+
     const nav_items = document.querySelector('.nav-items');
     nav_items.classList.toggle('active');
+
     const logo = document.querySelector('.logo');
     logo.classList.toggle('borrar_logo');
+        
 });
 
  
