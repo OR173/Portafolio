af6b4f838e38efc1ef27700b12eaa9e9086b3662
Orestes <oresmendez@gmail.com>
2022-09-13 15:51:26 -0500

    usando el segundo cherry-pick

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
   base: 'portafolioReact',
+=======
+>>>>>>> 1d263b5 (regresando a react)
   plugins: [react()]
 })
