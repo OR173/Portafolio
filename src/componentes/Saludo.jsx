1d263b53d3f74a36fbc0faeda4e4703486a6ddb0
Orestes <oresmendez@gmail.com>
2022-09-13 15:51:26 -0500

    regresando a react

---
+++ src/componentes/Saludo.jsx
@@ -0,0 +1,26 @@
+
+function Saludo() {   return( 
+
+    <section class="saludo contenedores">
+           
+        <div class="saludo__img ">
+            <img class="profile-image" />
+        </div>
+        <div class="saludo__text">
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, corporais ipsum aut fugiat
+                laboriosam molestiae eos sint quas neque, ad voluptatum numquam sapiente quae tenetur iste nemo,
+                totam nobis! Minus!</p>
+
+            <div class="saludo__btn">
+                <div><a class="main_btn" href="#contact">Contact me</a></div>
+                <div><a class="main_btn" href="" target="_blank">See my resume</a></div>
+
+            </div>
+
+        </div>
+    </section>
+    
+
+
+)}  export default Saludo;  
